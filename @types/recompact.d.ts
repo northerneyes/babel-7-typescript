14bc94fb4745b6774127ba1a5e94323f8ebeb272
George Buhanov <gbuhanov@gmail.com>
2018-09-11 22:09:02 +0300

    add types

---
+++ @types/recompact.d.ts
@@ -0,0 +1,21 @@
+///<reference types="recompose" />
+
+declare module 'recompact/compose' {
+  import { compose } from 'recompose'
+  export default compose
+}
+
+declare module 'recompact/withState' {
+  import { withState } from 'recompose'
+  export default withState
+}
+
+declare module 'recompact/withHandlers' {
+  import { withHandlers } from 'recompose'
+  export default withHandlers
+}
+
+declare module 'recompact/lifecycle' {
+  import { lifecycle } from 'recompose'
+  export default lifecycle
+}
