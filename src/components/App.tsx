b83972ac2fa9ab4a1049873d3c0a8ed420cbc965
George Buhanov <gbuhanov@gmail.com>
2018-09-13 21:25:43 +0300

    styled component example

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import { RecompactExample, PropExample } from './RecompactExample'
+import { StyledExample } from './StyledExample'
 import Example from './Example'
 import { IntlProvider } from 'react-intl'
 
@@ -16,6 +17,7 @@ class App extends Component<Props> {
           <Example />
           <RecompactExample name="20" />
           <PropExample name="2" />
+          <StyledExample name="some name" />
         </>
       </IntlProvider>
     )
---
+++ src/components/StyledExample.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+type Props = {
+  name: string
+}
+
+const Container = styled.div`
+  margin: 10px;
+`
+
+const Quote = styled.h1<{ color: string }>`
+  color: ${props => props.color};
+  font: 400 36px/1.4 'cardo';
+  font-style: italic;
+  font-weight: normal;
+  text-align: left;
+  text-indent: -10px;
+  max-width: 800px;
+  width: 80%;
+  margin: 0 auto;
+`
+
+type AProps = {
+  className?: string
+  count: number
+}
+
+function AnotherComponent(props: AProps) {
+  return <div className={props.className}>Another components</div>
+}
+
+const StyledAnotherComponent = styled(AnotherComponent)`
+  color: green;
+`
+
+export function StyledExample(props: Props) {
+  return (
+    <Container>
+      <Quote color="red">Some text</Quote>
+      <StyledAnotherComponent count={4} />
+      {props.name}
+    </Container>
+  )
+}
