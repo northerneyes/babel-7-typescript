0d30c8546c3bf89cbf44f5c72e130c931f963bd5
George Bukhanov <gbuhanov@gmail.com>
2019-06-03 22:35:58 +0300

    upgrade to ts@3.5.1

--- src/components/typesafe-get.ts
+++ src/components/typesafe-get.ts
@@ -12,22 +12,6 @@ export function get<
   S1 extends keyof Defined<T>,
   S2 extends keyof Prop<Defined<T>, S1>,
   S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>,
-  S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>,
-  S5 extends keyof Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>
->(
-  obj: T,
-  prop1: S1,
-  prop2: S2,
-  prop3: S3,
-  prop4: S4,
-  prop5: S5
-): Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>[S5] | undefined
-
-export function get<
-  T,
-  S1 extends keyof Defined<T>,
-  S2 extends keyof Prop<Defined<T>, S1>,
-  S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>,
   S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>
 >(
   obj: T,
